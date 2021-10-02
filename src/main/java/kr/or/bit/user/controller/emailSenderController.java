18d72f45cb73c392008ae727ec94a0ffe08e5ad1
qkrtkdwns3410 <qkrtkdwns3410@naver.com>
2021-10-02 14:25:46 +0900

    이메일 센더 클래스 이메일 비밀번호 보안문제떄문에.. 수정했습니다.

--- src/main/java/kr/or/bit/user/controller/emailSenderController.java
+++ src/main/java/kr/or/bit/user/controller/emailSenderController.java
@@ -91,9 +91,9 @@ class MyAuthentication extends Authenticator{
     
     public MyAuthentication(){
         
-        String id = "qkrtkdwns3410";       // 구글 ID
-        String pw = "Tkd1wns1!";          // 구글 비밀번호
-        
+        String id = "qkrtkdwns34102";       // 구글 ID
+        String pw = "qkrtkdwnsdlqslek!@#";          // 구글 비밀번호
+    
         // ID와 비밀번호를 입력한다.
         pa = new PasswordAuthentication(id, pw);
         
