a45efa712c53c403e5bbc230df95dc06a15863a1
yesolje <geenyou@naver.com>
2021-10-04 12:48:49 +0900

    10월 4일 12시 예솔 / 댓글 비동기 수정

---
+++ src/main/java/kr/or/bit/user/dto/Good_manage.java
@@ -0,0 +1,31 @@
+package kr.or.bit.user.dto;
+
+public class Good_manage {
+    private int idx;
+    private int no;
+    private String id;
+
+    public int getIdx() {
+        return idx;
+    }
+
+    public void setIdx(int idx) {
+        this.idx = idx;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
